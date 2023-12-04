90244673e73531b8d7415d1dfc520bf930b64a7b
camerondb <cameron@mollybet.com>
2023-12-04 13:06:33 +0000

    perf day4

--- src/solutions/day4.rs
+++ src/solutions/day4.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, HashMap};
+use std::{collections::{HashSet, HashMap}, time::Instant};
 
 use super::Solution;
 
@@ -27,24 +27,25 @@ impl Solution for Day4A {
     }
 }
 
-fn process_card(card_matches: &mut HashMap<usize, usize>, copies: &mut HashMap<usize, usize>, index: usize, card: &str) -> usize {
-    let points: usize = if let Some(points) = card_matches.get(&index) {
+fn process_card(copies: &mut HashMap<usize, usize>, cached_points: Option<&usize>, index: usize, card: &str) -> usize {
+    let points: usize = if let Some(points) = cached_points {
         *points
     } else {
         let (_, numbers) = card.split_once(": ").unwrap();
         let (winning, actual) = numbers.split_once(" | ").unwrap();
 
         let winning_numbers: HashSet<usize> = HashSet::from_iter(winning.split_whitespace().map(|n| n.parse::<usize>().unwrap()));
-        actual.split_whitespace().fold(0, |acc, n| {
+        let points = actual.split_whitespace().fold(0, |acc, n| {
             let n = n.parse::<usize>().unwrap();
             if winning_numbers.contains(&n) {
                 acc + 1
             } else {
                 acc
             }
-        })
+        });
+
+        points
     };
-    card_matches.insert(index, points);
 
     for i in 0..points {
         copies.entry(i+index+1).and_modify(|n| *n += 1).or_insert(1);
@@ -62,11 +63,16 @@ impl Solution for Day4B {
         let mut card_matches: HashMap<usize, usize> = HashMap::new();
         let cards: Vec<&str> = input.lines().collect();
         for (i, c) in cards.into_iter().enumerate() {
-            let points = process_card(&mut card_matches, &mut copies, i, c);
+            let cached_points = card_matches.get(&i);
+            let points = process_card(&mut copies, cached_points, i, c);
+            if let None = cached_points {
+                card_matches.insert(i, points);
+            }
+
             let n_copies = *copies.get(&i).unwrap_or(&0);
             if points > 0 {
                 for _ in 0..n_copies {
-                    process_card(&mut card_matches, &mut copies, i, c);
+                    process_card(&mut copies, Some(&points), i, c);
                 }
             }
 
